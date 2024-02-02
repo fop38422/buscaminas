123a8a139a525c880239fd5f97a0931484a7a812
angel <fop38422@educastur.es>
2024-02-02 13:34:53 +0100

    base html y css

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-let mapa = [10[10]];
+let mapa = [10];
 
-for (i = 1; i <= 10; i++){
-    
+for (let i = 0; i < 10; i++){
+    mapa[i] = [];
+    for (let j = 1; j <= 10; j++){
+       mapa[i][j] = 0;
+    }
+}
+
+for (let i = 0; i < 10; i++){
+    for (let j = 1; j <= 10; j++){
+        console.log(mapa[i][j]);
+    }
 }
